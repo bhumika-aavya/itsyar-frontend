ce139d474bda1e3c0da89f5e103e5c7f8f6f8cd3
bhumika-aavya <bhumika.balchandani@aavya.com>
2026-07-29 16:29:35 +0530

    fix: payment disabled button

--- src/pages/organizer/OrganizerDashboard.tsx
+++ src/pages/organizer/OrganizerDashboard.tsx
@@ -237,7 +237,7 @@ export default function OrganizerDashboard() {
                                     {needsPayment && (
                                         <button
                                             onClick={() => handlePurchase(h.id)}
-                                            disabled={!h.pricing || parseFloat(h.pricing) <= 0}
+                                            // disabled={!h.pricing || parseFloat(h.pricing) <= 0}
                                             title={!h.pricing || parseFloat(h.pricing) <= 0 ? "Payment amount is $0.00" : "Pay & Publish"}
                                             className="flex items-center gap-1.5 px-4 py-2.5 bg-amber-50 border border-amber-100 rounded-xl font-bold text-xs text-amber-700 hover:bg-amber-100 transition-all shadow-sm disabled:opacity-50 disabled:cursor-not-allowed"
                                         >
